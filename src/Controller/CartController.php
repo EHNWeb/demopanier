0f5f024f0a44e97196daa86833c44e6d00e45c26
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-30 11:40:54 +0200

    Add functio Add Product

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,30 @@ class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
         ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function add($id, RequestStack $rs)
+    {
+        // On va créer une SESSION grâce à la classe RequestStack
+        $session = $rs->getSession();
+
+        // On récupère l'attribut de SESSION cart s'il existe, sinon , on récupère un tableau vide
+        $cart = $session->get('cart', []);
+        // Le tableau cart contient les quantités commandées des produits
+
+        // Si le produit existe déjà dans le panier, on incrémente la quantité
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            // l'index du tableau cart est l'id du produit
+            $cart[$id] = 1;
+        }
+        // Je sauvegarde l'état de monm panier à l'attribut de session 'cart'
+        $session->set('cart', $cart);
+
+        dd($session->get('cart'));
     }
 }
