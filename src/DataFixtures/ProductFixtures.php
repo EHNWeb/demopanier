613a6fcadd30d9265010d1f789f621b376d22eb2
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-30 10:06:13 +0200

    Define Mixture Product

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for($i=0; $i <= mt_rand(8, 10); $i++)
+        {
+            $product = new Product();
+
+            $product->setTitle($faker->sentence(3, false))
+                    ->setImage($faker->imageUrl)
+                    ->setPrice($faker->randomFloat(2, 10, 100));
+        }
 
         $manager->flush();
     }
